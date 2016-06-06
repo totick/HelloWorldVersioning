f6ba628bad48189355f786683c91b2ef178f6f9f
John Lundgren <john.paul.lundgren@gmail.com>
2016-06-06 17:18:19 +0200

    Second change in develop after release 1.0

--- HelloWorldVersioning/src/main/java/org/examples/main/Main.java
+++ HelloWorldVersioning/src/main/java/org/examples/main/Main.java
@@ -3,7 +3,7 @@ package org.examples.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("First change in develop after release 1.0");
+		System.out.println("Second change in develop after release 1.0");
 	}
 
 }
