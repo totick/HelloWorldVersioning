d6a9bb7900e35052d90f974c4136ac317021635a
John Lundgren <john.paul.lundgren@gmail.com>
2016-06-06 17:24:53 +0200

    Release 2.0

--- HelloWorldVersioning/src/main/java/org/examples/main/Main.java
+++ HelloWorldVersioning/src/main/java/org/examples/main/Main.java
@@ -3,7 +3,7 @@ package org.examples.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Second change in develop after release 1.0");
+		System.out.println("Release 2.0");
 	}
 
 }
