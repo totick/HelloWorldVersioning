28f6725f004eaa7b1e48443475e6b21c3530072f
John Lundgren <john.paul.lundgren@gmail.com>
2016-06-06 12:24:46 +0200

    Release 1.0

--- HelloWorldVersioning/src/main/java/org/examples/main/Main.java
+++ HelloWorldVersioning/src/main/java/org/examples/main/Main.java
@@ -3,7 +3,7 @@ package org.examples.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Second commit in develop branch");
+		System.out.println("Commit to be added to release branch");
 	}
 
 }
