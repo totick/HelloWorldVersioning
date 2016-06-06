4a1ac0e516edef18b2e476270a115e8181db916b
John Lundgren <john.paul.lundgren@gmail.com>
2016-06-06 12:13:38 +0200

    Second commit in develop branch

--- HelloWorldVersioning/src/main/java/org/examples/main/Main.java
+++ HelloWorldVersioning/src/main/java/org/examples/main/Main.java
@@ -3,7 +3,7 @@ package org.examples.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("First commit in develop branch");
+		System.out.println("Second commit in develop branch");
 	}
 
 }
